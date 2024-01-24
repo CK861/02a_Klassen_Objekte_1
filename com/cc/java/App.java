5ae2a8b4ff6334bed58c47dd550ab3a6b8fabd55
BK <ck.comcave@proton.me>
2024-01-24 10:52:05 +0100

    cAt aPP

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,8 +3,19 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
+    
+        Cat cat = new Cat();  //Instanziierung
 
+        System.out.println(cat);
+        
+        // cat.sayHello();
+        cat.tellYourAddress();
 
+        System.out.println("---------------");
+
+        Cat cat2 = new Cat();
+        System.out.println(cat2);
+        cat2.tellYourAddress();
     }
 
    
---
+++ com/cc/java/Cat.java
@@ -0,0 +1,16 @@
+package com.cc.java;
+
+public class Cat {
+
+public void sayHello()
+{
+    System.out.println("HELLLOOOO");
+}
+
+public void tellYourAddress()
+{
+    System.out.println(this);
+
+}
+
+}
