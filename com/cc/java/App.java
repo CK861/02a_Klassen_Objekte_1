6e472fa58bf811a00149f9ae0f2435283ba6ac2b
BK <ck.comcave@proton.me>
2024-01-24 11:48:55 +0100

    Objektzuweisung

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,13 +4,24 @@ public class App {
     
     public static void main(String[] args) {
     
+        // Objekt erzeugen
         Cat cat = new Cat();  //Instanziierung
-
         System.out.println(cat);
         
+        // Wertzuweisung
+        cat.name = "Alonzo";
+        cat.furColor = "grey";
+        cat.age = 35;
+        
+        // Ausgabe
+        System.out.println(cat.name);
+        System.out.println(cat.furColor);
+        System.out.println(cat.age);
+
         // cat.sayHello();
         cat.tellYourAddress();
-
+        cat.tellYourName();
+        System.out.println(cat.age);
         System.out.println("---------------");
 
         Cat cat2 = new Cat();
