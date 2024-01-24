494b7d1912a2858a42319b98a1015904adf853c5
BK <ck.comcave@proton.me>
2024-01-24 11:50:57 +0100

    Schidt version Wertzuweisung

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,33 +3,40 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-    
+
         // Objekt erzeugen
-        Cat cat = new Cat();  //Instanziierung
+        Cat cat = new Cat(); 
         System.out.println(cat);
-        
+
         // Wertzuweisung
         cat.name = "Alonzo";
         cat.furColor = "grey";
         cat.age = 35;
-        
+
         // Ausgabe
         System.out.println(cat.name);
         System.out.println(cat.furColor);
         System.out.println(cat.age);
 
-        // cat.sayHello();
-        cat.tellYourAddress();
-        cat.tellYourName();
-        System.out.println(cat.age);
         System.out.println("---------------");
 
         Cat cat2 = new Cat();
         System.out.println(cat2);
-        cat2.tellYourAddress();
+      
+        // Wertzuweisung
+        cat2.name = "Grizabella";
+        cat2.furColor = "white";
+        cat2.age = 29;
+
+        // Ausgabe
+        System.out.println(cat2.name);
+        System.out.println(cat2.furColor);
+        System.out.println(cat2.age);
+        
+
+
     }
 
    
 
 }
-
